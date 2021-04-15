59a7941d4c80f376419b5c6d1e1f2866d8fbea91
AnthonyRodriguez93 <52090741+AnthonyRodriguez93@users.noreply.github.com>
2021-04-15 13:51:56 -0500

    (Added bonus section) a button that randomizes the color for background and both color pickers

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 var style = getComputedStyle(body);
 var backgroundColor = style.backgroundImage;
-
+var btn = document.querySelector(".random");
 
 function setGradient() {
 	body.style.background = 
@@ -16,6 +17,16 @@ function setGradient() {
 
 	css.textContent = body.style.background + ";";
 }
+
+function random(){
+	color1.value = "#"+Math.floor(Math.random()*16777215).toString(16);
+	color2.value = "#"+Math.floor(Math.random()*16777215).toString(16);
+	css.textContent = color1.value + " " + color2.value+";";
+
+	setGradient();
+}
+
+btn.addEventListener("click", random);
 
 css.textContent = backgroundColor + ";";
 
